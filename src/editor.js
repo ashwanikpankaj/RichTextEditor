0b8e4b8eaf63d216b142393ed2dcdf1bb84ccfd6
ashwani <akpankaj@tekion.com>
2024-01-21 17:19:44 +0530

    editir all formatting options setup

--- src/editor.js
+++ src/editor.js
@@ -7,6 +7,39 @@ const ACTIVE_CLASS_MAP = {
 
 const optionsButton = document.querySelectorAll(".option-button");
 const editableDiv = document.querySelector("#editable");
+editableDiv.focus();
+const elementTypeSelect = document.querySelector("#elementType");
+const selectFontType = document.querySelector("#selectFontType");
+const colorPicker = document.querySelector("#colorPicker");
+const highLighter = document.querySelector("#highLighter");
+
+// creating option for elementType
+const elementTagOptions = [
+  { title: "H1", value: "h1" },
+  { title: "H2", value: "h2" },
+  { title: "H3", value: "h3" },
+  { title: "H4", value: "h4" },
+  { title: "H5", value: "h5" },
+  { title: "H6", value: "h6" },
+  { title: "P", value: "p" },
+];
+
+let fontList = [
+  "Arial",
+  "Verdana",
+  "Times New Roman",
+  "Garamond",
+  "Georgia",
+  "Courier New",
+  "cursive",
+];
+
+// giving the default value to select tag
+
+elementTypeSelect.value = "h1";
+elementTypeSelect.textContent = "H1";
+selectFontType.value = fontList[0];
+selectFontType.textContent = fontList[0];
 
 function removeHighlighter(el) {
   optionsButton.forEach((item) => {
@@ -19,12 +52,35 @@ function removeHighlighter(el) {
   });
 }
 
+elementTypeSelect.addEventListener("change", () => {
+  const tagName = elementTypeSelect.value;
+  modifyText("formatBlock", false, tagName);
+  focusEditableDiv();
+});
+
+selectFontType.addEventListener("change", () => {
+  modifyText("fontName", false, selectFontType.value);
+  focusEditableDiv();
+});
+
+fontList.forEach((item) => {
+  const option = document.createElement("option");
+  option.textContent = item;
+  option.value = item;
+  selectFontType.appendChild(option);
+});
+
+elementTagOptions.forEach((item) => {
+    const option = document.createElement("option");
+    option.textContent = item?.value;
+    option.value = item?.value;
+    elementTypeSelect.appendChild(option);
+  });
+
 // for focussing the div when clicking on the editable div
 function focusEditableDiv() {
   editableDiv.focus();
 }
-
-let savedSelection = null;
 
 // adding event listener to all formatting button
 optionsButton.forEach((button) => {
@@ -33,10 +89,35 @@ optionsButton.forEach((button) => {
     modifyText(button.id, false, null);
     removeHighlighter(button);
     focusEditableDiv();
+    const selection = window.getSelection().getRangeAt(0);
+    console.log({selection})
   });
 });
 
 const modifyText = (command, defaultUi, value) => {
-    console.log({command, defaultUi, value})
   document.execCommand(command, defaultUi, value);
 };
+
+function selectHighlighter() {
+  const highlightColor = highLighter?.value;
+  modifyText("hiliteColor", false, highlightColor);
+  focusEditableDiv();
+}
+
+function selectFontColor() {
+  const fontColor = colorPicker?.value;
+  modifyText("foreColor", false, fontColor);
+  focusEditableDiv();
+}
+
+// function createRange() {
+//     // below code is settng the range of the selected 
+//   const range = document.createRange();
+//   range.selectNode(editableDiv);
+//   range.collapse(false);
+
+//   // create an new range 
+//   const newRange = document.createRange();
+//   newRange.setStartAfter()
+
+// }
